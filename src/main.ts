142a75f5f3d3947860371a5915f0eac40cfe32b3
Diego Gaspar <diegogaspar.dev.br>
2023-08-12 00:23:05 -0300

    feat: config eslint and prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import cors from '@fastify/cors'
+import helmet from '@fastify/helmet'
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import fastifyFileUpload from 'fastify-file-upload'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
+
+  app.register(cors)
+
+  app.register(fastifyFileUpload, {
+    limits: { fileSize: 1024 * 1024 * 5 },
+    useTempFiles: true,
+    tempFileDir: 'tmp',
+    createParentPath: true,
+    uriDecodeFileNames: true,
+    safeFileNames: '/.(jpg|jpeg|png)$/i',
+    preserveExtension: true
+  })
+  await app.register(helmet)
+
+  const config = new DocumentBuilder()
+    .setTitle('API Uai-food')
+    .setDescription('API para a rede de lojas da UAI-food')
+    .setVersion('1.0.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { groups: ['transform'] }
+    })
+  )
+
+  await app.listen(process.env.PORT, '0.0.0.0')
 }
-bootstrap();
+bootstrap()
